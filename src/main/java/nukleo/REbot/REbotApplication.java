17fc39818d977e792641da4fbe01ee4618b66c0f
Denis <nukleoyt@gmail.com>
2025-10-04 16:20:17 +0200

    Primo commit

---
+++ src/main/java/nukleo/REbot/REbotApplication.java
@@ -0,0 +1,13 @@
+package nukleo.REbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class REbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(REbotApplication.class, args);
+	}
+
+}
