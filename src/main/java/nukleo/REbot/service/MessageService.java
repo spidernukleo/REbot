c4b5835d788d57d527c4cf0133bf6ab587071955
Denis <nukleoyt@gmail.com>
2025-10-05 18:08:33 +0200

    finito primo setup

--- src/main/java/nukleo/REbot/service/MessageService.java
+++ src/main/java/nukleo/REbot/service/MessageService.java
@@ -4,6 +4,8 @@ package nukleo.REbot.service;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 @Service
@@ -12,17 +14,25 @@ public class MessageService {
 
     private final TelegramService telegramService;
 
-    public void handleMessage(Map<String, Object> message) {
+    private Long getChatId(Map<String, Object> message){
         @SuppressWarnings("unchecked")
         Map<String, Object> chat = (Map<String, Object>) message.get("chat");
-        Long chatId = ((Number) chat.get("id")).longValue();
+        return ((Number) chat.get("id")).longValue();
+    }
 
+    public void handleMessage(Map<String, Object> message) {
+        Map<String, Runnable> commands = new HashMap<>();
+        commands.put("/topdaniele".toLowerCase(), () -> handleTop(message));
+        commands.put("daniele".toLowerCase(), () -> handleDaniele(message)); //mappa con tutti i comandi e che funzione eseguire
 
-        if(message.get("text").equals("/start")){
-            telegramService.sendMessage(chatId, "BENVENUTO");
-        }
-        else{
-            telegramService.sendMessage(chatId, "Comando sconosciuto");
-        }
+        commands.getOrDefault(((String) message.get("text")).toLowerCase(), ()->{}).run(); //se non trovato non lanciare nulla
+    }
+
+    public void handleTop(Map<String, Object> message){
+        telegramService.sendMessage(getChatId(message), "ecco la top");
+    }
+
+    public void handleDaniele(Map<String, Object> message){
+        telegramService.sendMessage(getChatId(message), "sei diventato");
     }
 }
